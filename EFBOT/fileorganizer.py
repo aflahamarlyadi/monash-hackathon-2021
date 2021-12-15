bb2d06ee266a5d7ba93e1916c8e13b298732e59d
Aflah Hanif Amarlyadi <89532038+aflahamarlyadi@users.noreply.github.com>
2021-12-15 12:12:16 +0400

    Update fileorganizer.py

--- EFBOT/fileorganizer.py
+++ EFBOT/fileorganizer.py
@@ -1,9 +1,8 @@
-# importing os and shutil module  
 import os, shutil, time
 from pathlib import Path
+from filecmp import cmp
 from datetime import datetime
 from extensions import extension_paths
-
 
 
 def add_cdate_to_path(child, path: Path):
@@ -11,10 +10,39 @@ def add_cdate_to_path(child, path: Path):
     Helper function that adds current month and year to destination path.
     """
     date = datetime.strptime(time.ctime(os.path.getctime(child)), "%a %b %d %H:%M:%S %Y")
-    dated_path = path / f'{date.strftime("%b")} {date.year}'
-    dated_path.mkdir(parents=True, exist_ok=True)
+    dated_path = path.joinpath(f'{date.strftime("%b")} {date.year}')
     return dated_path
 
+
+def remove_duplicates(source):
+    """
+    Helper function that check for duplicates in a directory
+    """
+    source_path = Path(source)
+    files = os.listdir(source_path)
+
+    duplicates = []
+
+    for file in files:
+        
+        is_duplicate = False
+        
+        for class_ in duplicates:
+            is_duplicate = cmp(
+                source_path / file,
+                source_path / class_[0],
+                shallow = False
+            )
+            if is_duplicate:
+                class_.append(file)
+                break
+        
+        if not is_duplicate:
+            duplicates.append([file])
+
+    for class_ in duplicates:
+        for file in class_[:-1]:
+            os.remove(source_path / file)
 
 
 def organize_by_type(source, destination):
@@ -28,13 +56,21 @@ def organize_by_type(source, destination):
     source_path = Path(source)
     destination_path = Path(destination)
     destination_root = Path(destination)
+    message = ""
+
     for child in source_path.iterdir():
         if child.is_file() and child.suffix.lower() in extension_paths:
-            destination_path = destination_root / extension_paths[child.suffix.lower()]
+            destination_path = destination_root.joinpath(extension_paths[child.suffix.lower()])
             destination_path = add_cdate_to_path(child, path=destination_path)
-            shutil.move(src=child, dst=destination_path)
-            print(f'{child.name} has successfully been moved to {destination_path}')
+            if os.path.exists(destination_path.joinpath(child.name)):
+                message += f'Duplicate of {child.name} found in {destination_path}\n'
+                os.remove(child)
+            else:
+                destination_path.mkdir(parents=True, exist_ok=True)
+                shutil.move(src=child, dst=destination_path)
+                message += f'{child.name} has successfully been moved to {destination_path}\n'
 
+    return message
 
 
 def organize_by_keyword(source, destination, keywords):
@@ -45,11 +81,21 @@ def organize_by_keyword(source, destination, keywords):
     source_path = Path(source)
     destination_path = Path(destination)
     destination_root = Path(destination)
+    organized_files = set()
+    message = ""
+
     for child in source_path.iterdir():
         if child.is_file():
             for keyword in keywords:
                 if keyword.upper() in child.name.upper():
-                    destination_path = destination_root / keyword
+                    destination_path = destination_root.joinpath(keyword)
                     destination_path.mkdir(parents=True, exist_ok=True)
                     shutil.copy(src=child, dst=destination_path)
-                    print(f'{child.name} has successfully been copied to {destination_path}')
+                    organized_files.add(child)
+                    message += f'{child.name} has successfully been moved to {destination_path}\n'
+    
+    for file in organized_files:
+        if os.path.exists(file):
+            os.remove(file)
+
+    return message
